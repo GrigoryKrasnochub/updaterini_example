2dfa0e87575252886cc2928aaca8e4e4cc89f36d
Grigory Krasnochub <kgs@ht.ru>
2021-07-14 20:47:42 +0300

    1.0.1 rel
    # Conflicts:
    #	main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	fmt.Println("Example file")
+	fmt.Println("Example file 1.0.1 branch")
 	fmt.Println("Press any key for update")
 	bufio.NewReader(os.Stdin).ReadByte()
 	fmt.Println("Success!")
